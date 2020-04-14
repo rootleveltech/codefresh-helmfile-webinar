cd1ad6199ff9ee38518ebc305da14a2a050d075b
Braden Wright <braden.wright@rootleveltech.com>
2020-04-13 21:53:19 -0500

    Save point

---
+++ code-02/helm/charts/traefik-public/get_domain.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+INGRESS_IP=`kubectl get svc -n dev-system dev-traefik-public -o jsonpath='{.status.loadBalancer.ingress[*].ip}' 2> /dev/null`
+exit_status=$?
+if [ $exit_status -eq 0 ]; then
+  INGRESS_IP="$INGRESS_IP.xip.io"
+else
+  INGRESS_IP=''
+fi
+
+echo $INGRESS_IP
