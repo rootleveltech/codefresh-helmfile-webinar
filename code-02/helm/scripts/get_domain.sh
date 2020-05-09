d16e3e83483d014b4215e1625b3329b54634fdee
Braden Wright <braden.wright@rootleveltech.com>
2020-05-09 16:28:57 -0500

    save me

--- code-02/helm/scripts/get_domain.sh
+++ code-02/helm/scripts/get_domain.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-ENV=$1
-
-INGRESS_IP=`kubectl get svc -n ${ENV}-system ${ENV}-traefik-public -o jsonpath='{.status.loadBalancer.ingress[*].ip}' 2> /dev/null`
+INGRESS_IP=`kubectl get svc -n system traefik-public -o jsonpath='{.status.loadBalancer.ingress[*].ip}' 2> /dev/null`
 exit_status=$?
 if [ $exit_status -eq 0 ]; then
   if [ $INGRESS_IP ]; then
